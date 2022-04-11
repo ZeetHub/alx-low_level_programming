5ee45d4446b81b305574753f8159bda989cdbdea
Zerihun Eshete <zerihun.tilahun.td3767@gmail.com>
2022-04-10 17:54:19 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints a continuation of the previous statement.
+ * Return: 0 if all is well.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
