36a67ba244787b8eeea6f88022df300a1da2d4df
Zerihun Eshete <zerihun.tilahun.td3767@gmail.com>
2022-04-10 17:38:04 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - prints a string of characters.
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
