978a15253d8075adfa8ccc68379eb6faae040f5d
Zerihun Eshete <zerihun.tilahun.td3767@gmail.com>
2022-04-13 19:25:28 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result.
+ * @i: first integer to be added to j
+ * @j: second integer to be added to i
+ * Return: sum of i and j
+ */
+int add(int i, int j)
+{
+	int sum;
+
+	sum = i + j;
+
+	return (sum);
+}
