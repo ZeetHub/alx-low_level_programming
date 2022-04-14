a11aaa124b74bfffbb4c234e2d765a74d5060e08
Zerihun Eshete <zerihun.tilahun.td3767@gmail.com>
2022-04-13 18:25:11 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
  *_islower - verifies if a character is lower case
- *ch: is the character to be checked
+ *@ch: is the character to be checked
  *Return: 1(true) if ch is lowercase, 0(false) if not.
  */
 int _islower(int ch)
