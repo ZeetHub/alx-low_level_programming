293bc701c2e83477f749fff9b3a1f9c4a2cacdb6
Zerihun Eshete <zerihun.tilahun.td3767@gmail.com>
2022-04-13 18:02:11 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the , in lowercase,
+ * followed by a new line.
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int j;
+
+	j = 0;
+
+	while (j < 10)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		j++;
+	}
+}
