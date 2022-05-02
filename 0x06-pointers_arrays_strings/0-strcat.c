cf3fa44ec6de54c3b58eabcff2e9feef3c9d73e7
Zerihun Eshete <zerihun.tilahun.td3767@gmail.com>
2022-05-02 15:20:00 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: pointer to the concatenated string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while(dest[i] != '\0')
+		i++;
+
+	for(;src[j] != '\0'; i++, j++)
+	{
+		dest[i+1] = src[j];
+	}
+	putchar('\0');
+	return (dest);
+}
