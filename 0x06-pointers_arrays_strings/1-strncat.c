65d1c74f5e67abb794d075059ac08858c568927e
Zerihun Eshete <zerihun.tilahun.td3767@gmail.com>
2022-05-02 16:12:43 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,12 +13,13 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int i;
+	int j;
 
 	i = 0;
 	while (dest[i] != '\0')
 		i++;
 
-	for (int j = 0; src[j] != '\0' && j < n; i++, j++)
+	for (j = 0; src[j] != '\0' && j < n; i++, j++)
 	{
 		dest[i] = src[j];
 	}
