5bb7e9b605f07ac1134cbf03ed81f49fe5965c3a
Zerihun Eshete <zerihun.tilahun.td3767@gmail.com>
2022-05-02 17:15:57 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,14 +14,18 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 
 	i = 0;
-	for (; scr[i] != '\0' && i < n; i++)
+
+	while (src[i] != '\0' && i < n)
 	{
 		dest[i] = src[i];
+		i++;
 	}
-	if (i < n)
-		for (; i < n; i++)
-			dest[i] = '\0';
-	else if (src[i] == '\0')
-		dest[i] = src[i];
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
 	return (dest);
 }
