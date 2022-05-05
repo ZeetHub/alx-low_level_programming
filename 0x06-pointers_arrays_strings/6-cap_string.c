b7a1abc37d86ed60a6ddbcfd710e0a0df164890a
Zerihun Eshete <zerihun.tilahun.td3767@gmail.com>
2022-05-05 11:52:23 -0700

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,7 +11,7 @@ char *cap_string(char *s)
 {
 	int i, j;
 
-	char spe[13] = {' ', '\t', '\n', ',', ';', '.',
+	char s_char[13] = {' ', '\t', '\n', ',', ';', '.',
 		'!', '?', '"', '(', ')', '{', '}'};
 
 	i = 0;
@@ -22,7 +22,7 @@ char *cap_string(char *s)
 
 		for (j = 0; j < 13; j++)
 		{
-			if (s[i] == spe[j])
+			if (s[i] == s_char[j])
 			{
 				if (s[i + 1] >= 97 && s[i + 1] <= 122)
 					s[i + 1] -= 32;
