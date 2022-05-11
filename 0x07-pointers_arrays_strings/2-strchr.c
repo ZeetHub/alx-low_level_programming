71b1be2b885fad373769132c9046724b0ce751a9
Zerihun Eshete <zerihun.tilahun.td3767@gmail.com>
2022-05-11 12:24:31 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,18 +11,10 @@
 
 char *_strchr(char *s, char c)
 {
-	int a;
-
-	while (1)
-	{
-		a = *s++;
-		if (a == c)
-		{
-			return (s - 1);
-		}
-		if (a == 0)
-		{
-			return (NULL);
-		}
-	}
+	while (*s != c && *s != '\0')
+		s++;
+	if (*s == c)
+		return (s);
+	else if (*s == '\0')
+		return (NULL);
 }
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,9 +1,4 @@
-#ifndef ALX_H
-#define ALX_H
-
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
-
-#endif
