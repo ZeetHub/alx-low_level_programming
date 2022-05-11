6b901f0d1a90c8f13d3114686e7769cc43cb3898
Zerihun Eshete <zerihun.tilahun.td3767@gmail.com>
2022-05-11 06:48:23 -0700

    memset

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,7 +2,7 @@
 #define ALX_H
 
 int _putchar(char c);
-#include char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif
