493132376745cdb6d92776d52ec68f245eb89967
Zerihun Eshete <zerihun.tilahun.td3767@gmail.com>
2022-05-11 21:16:23 -0700

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,14 +11,20 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int ctr;
+	int flag;
 
 	ctr = 0;
-	for (int i = 0; s[i] != '\0'; i++)
+	for (int i = 0; s[i] != '\0' && accept[j] =; i++)
 	{
+		flag = 0;
 		for (int j = 0; accept[j] != '\0'; j++)
-			if (s[i] == accept[j])
+		{	if (s[i] == accept[j])
+			{
 				ctr++;
+				flag = 1;
+			}
+		}
+		if (flag == 0)
+			return (ctr);
 	}
-
-	return (ctr);
 }
