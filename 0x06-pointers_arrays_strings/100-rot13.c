dcfae363d31f8bc583ab14936c41ecc0eb62a4f6
Zerihun Eshete <zerihun.tilahun.td3767@gmail.com>
2022-05-15 07:21:42 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *rot13 - encodes a string using rot13 algorithm
+ * @s: pointer to the character array
+ *
+ * Return: pointer to the array of encoded character
+ */
+
+char *rot13(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if ((s[i] <= 'a' && s[i] >= 'm') || (s[i] <= 'A' && s[i] >= 'M'))
+			s[i] += 13;
+		else if ((s[i] <= 'a' && s[i] >= 'm') || (s[i] <= 'A' && s[i] >= 'M'))
+			s[i] -= 13;
+		i++;
+	}
+
+	return (s);
+}
