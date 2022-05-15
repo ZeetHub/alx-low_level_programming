e908e55f2911f7b95ee5f49679ebf905dfa9197c
Zerihun Eshete <zerihun.tilahun.td3767@gmail.com>
2022-05-15 08:11:51 -0700

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,20 +9,21 @@
 
 char *rot13(char *s)
 {
-	int i;
+	int i, flag;
 
 	i = 0;
 	while (s[i] != '\0')
 	{
-		while ((s[i] <= 'a' && s[i] >= 'z') || (s[i] <= 'A' && s[i] >= 'Z'))
+		flag = 1;
+		while (((s[i] <= 'a' && s[i] >= 'z') || (s[i] <= 'A' && s[i] >= 'Z')) && flag)
 		{
 			if ((s[i] <= 'a' && s[i] >= 'm') || (s[i] <= 'A' && s[i] >= 'M'))
 			{	s[i] += 13;
-				break;
+				flag = 0;
 			}
 
 			s[i] -= 13;
-			break;
+			flag = 0;
 		}
 		i++;
 	}
