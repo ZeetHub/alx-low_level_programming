fd6d5f078b5a6610b220cb8e32a27b2961f51ec7
Zerihun Eshete <zerihun.tilahun.td3767@gmail.com>
2022-05-15 11:59:50 -0700

    Numbers have life; they're not just symbols on paper

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define PTRS_H
 
 #include <stdio.h>
+int _putchar(char c);
 char *_strcat(char *dest, char *str);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
