bc49370592719a3aec9cb4527d91534268e45687
Zerihun Eshete <zerihun.tilahun.td3767@gmail.com>
2022-05-15 13:21:48 -0700

    Numbers have life; they're not just symbols on paper

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -62,7 +62,7 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		putchar('-');
+		_putchar('-');
 		n *= -1;
 	}
 	while (i <= digits)
@@ -76,7 +76,7 @@ void print_number(int n)
 			div = n % 10;
 
 		ch = div + '0';
-		putchar(ch);
+		_putchar(ch);
 		i++;
 	}
 }
